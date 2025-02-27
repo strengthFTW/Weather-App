03764aa7c4334973cb6a6682bcb1fb76fd09c326
strengthFTW <riteshpashikanti321@gmail.com>
2025-02-27 14:58:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,39 +5,26 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-
 async function checkweather(city) {
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
     const data = await response.json();
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".Humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-    if(data.weather[0].main == "Clouds") {
-        weatherIcon.src ="images/clouds.png"
-    }
-    else if(data.weather[0].main == "Rain") {
-        weatherIcon.src ="images/rain.png"
-    }
-    else if(data.weather[0].main == "Drizzle") {
-        weatherIcon.src ="images/drizzle.png"
-    }
-    else if(data.weather[0].main == "Mist") {
-        weatherIcon.src ="images/mist.png"
-    }
-    else if(data.weather[0].main == "Clear") {
-        weatherIcon.src ="images/clear.png"
-    }
-    
+    // Use the weather icon code provided by OpenWeather
+    const iconCode = data.weather[0].icon;
+    weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}.png`; // Using icon code from OpenWeather API
+
     document.querySelector(".weather").style.display = "block";
-    
 }
 
-searchBtn.addEventListener("click" ,()=>{
+searchBtn.addEventListener("click", () => {
     checkweather(searchBox.value);
-} );
+});
 
-checkweather();
+// Optionally, you can add a default city check when the page loads (if required).
+// checkweather('London');
